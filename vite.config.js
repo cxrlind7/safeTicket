63ffa88f813fe463232f88ea0e67a672ad99c3c5
DigitalesBack Desarrollo <digitalesback-desarrollo@devsecops.hdi.com.mx>
2026-02-09 16:02:45 -0600

    feat: Add .nojekyll files to disable Jekyll processing.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/safeTicket/",
+  build: {
+    outDir: "docs",
+  },
   plugins: [vue()],
 });
